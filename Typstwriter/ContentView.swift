d7eeffc3e5f727cd1f7674a9ff5293d0e1f65703
Malted <malted@malted.dev>
2026-05-12 15:56:12 +0100

    Initial Commit

---
+++ Typstwriter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Typstwriter
+//
+//  Created by Ben Dixon on 5/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Typstwriter/TypstwriterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypstwriterApp.swift
+//  Typstwriter
+//
+//  Created by Ben Dixon on 5/12/26.
+//
+
+import SwiftUI
+
+@main
+struct TypstwriterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
